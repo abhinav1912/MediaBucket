7db994042f1abcc59b596682d4c00ae0b88961b7
abhinav1912 <abhinavmathur1912@gmail.com>
2022-08-13 00:50:51 +0530

    wip: added temporary HomeViewItem

---
+++ MediaBucket/HomeView/HomeViewItem.swift
@@ -0,0 +1,15 @@
+//
+//  HomeViewItem.swift
+//  MediaBucket
+//
+//  Created by Abhinav Mathur on 13/08/22.
+//
+
+import Foundation
+import UIKit
+
+struct HomeViewItem {
+    let title: String
+    let image: UIImage?
+    let description: String?
+}
