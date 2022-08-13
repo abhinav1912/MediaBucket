32c90272d3c57282a7408374c5253feebc174f92
abhinav1912 <abhinavmathur1912@gmail.com>
2022-08-13 11:23:57 +0530

    wip: added CollectionView to HomeViewController

--- MediaBucket/HomeView/HomeViewController.swift
+++ MediaBucket/HomeView/HomeViewController.swift
@@ -8,11 +8,30 @@
 import UIKit
 
 final class HomeViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    private var collectionView: UICollectionView
+    
+    init() {
+        let layout = UICollectionViewLayout()
+        self.collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 
-
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(self.collectionView)
+        self.collectionView.translatesAutoresizingMaskIntoConstraints = false
+        let layoutGuide = self.view.safeAreaLayoutGuide
+        let constraints = [
+            self.collectionView.topAnchor.constraint(equalTo: layoutGuide.topAnchor),
+            self.collectionView.bottomAnchor.constraint(equalTo: layoutGuide.bottomAnchor),
+            self.collectionView.leadingAnchor.constraint(equalTo: layoutGuide.leadingAnchor),
+            self.collectionView.trailingAnchor.constraint(equalTo: layoutGuide.trailingAnchor)
+        ]
+        NSLayoutConstraint.activate(constraints)
+    }
 }
 
