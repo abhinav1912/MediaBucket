2ec2d8676ce53e7266a3fafb1ddfd6df95c282a9
abhinav1912 <abhinavmathur1912@gmail.com>
2022-08-20 01:43:11 +0530

    wip: added ListItem

---
+++ MediaBucket/ListView/ListItem.swift
@@ -0,0 +1,14 @@
+//
+//  ListItem.swift
+//  MediaBucket
+//
+//  Created by Abhinav Mathur on 20/08/22.
+//
+
+import Foundation
+
+struct ListItem {
+    let title: String
+    let description: String?
+    let link: URL
+}
