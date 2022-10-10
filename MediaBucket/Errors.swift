8773cc6dfc867364f97f321db9cc53ca985aa744
abhinav1912 <abhinavmathur1912@gmail.com>
2022-10-10 21:02:02 +0530

    feat: added Errors enum

---
+++ MediaBucket/Errors.swift
@@ -0,0 +1,41 @@
+//
+//  Errors.swift
+//  MediaBucket
+//
+//  Created by Abhinav Mathur on 10/10/22.
+//
+
+import Foundation
+
+enum Errors {
+    case invalidFolderName
+    case folderNameExists
+
+    private enum TitleConstants {
+        static let invalidFolderName = "Invalid folder name"
+        static let folderNameExists = "Folder exists"
+    }
+
+    private enum DescriptionConstants {
+        static let invalidFolderName = "Folder name must have more than 6 characters"
+        static let folderNameExists = "The folder name already exists, please choose another name"
+    }
+
+    var title: String {
+        switch self {
+        case .invalidFolderName:
+            return TitleConstants.invalidFolderName
+        case .folderNameExists:
+            return TitleConstants.folderNameExists
+        }
+    }
+
+    var message: String {
+        switch self {
+        case .invalidFolderName:
+            return DescriptionConstants.invalidFolderName
+        case .folderNameExists:
+            return DescriptionConstants.folderNameExists
+        }
+    }
+}
